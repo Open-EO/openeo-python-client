37f55fdc819e2f0c1e461b5a7ed776c629d8fe9a
Bernhard Gößwein <bernhard.goesswein@geo.tuwien.ac.at>
2018-06-26 04:45:46 -0400

    add minor documentation improvement

--- openeo/job.py
+++ openeo/job.py
@@ -1,13 +1,14 @@
 from abc import ABC
 
+
 class Job(ABC):
     """Represents the result of creating a new Job out of a process graph. Jobs are stored in the
     backend and can be executed directly (in batch), or evaluated lazily."""
 
-    def __init__(self, job_id:str):
+    def __init__(self, job_id: str):
         self.job_id = job_id
 
-    def download(self, outputfile:str, outputformat:str):
+    def download(self, outputfile: str, outputformat: str):
         """ Download the result as a raster."""
         pass
 
@@ -18,3 +19,4 @@ class Job(ABC):
 
     def queue(self):
         """ Queues the job. """
+        pass
