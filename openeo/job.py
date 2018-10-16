af04ce1781847869ec5f40ae93d1ea8ddf5f91d2
Bernhard Gößwein <bernhard.goesswein@geo.tuwien.ac.at>
2018-10-16 14:33:56 +0200

    created new classes according to client guidelines

--- openeo/job.py
+++ openeo/job.py
@@ -15,6 +15,49 @@ class Job(ABC):
     def __init__(self, job_id: str):
         self.job_id = job_id
 
+    def describe_job(self):
+        """ Get all job information."""
+        # GET /jobs/{job_id}
+        pass
+
+    def update_job(self, process_graph=None, output_format=None,
+                   output_parameters=None, title=None, description=None,
+                   plan=None, budget=None, additional=None):
+        """ Update a job."""
+        # PATCH /jobs/{job_id}
+        pass
+
+    def delete_job(self):
+        """ Delete a job."""
+        # DELETE /jobs/{job_id}
+        pass
+
+    def estimate_job(self):
+        """ Calculate an time/cost estimate for a job."""
+        # GET /jobs/{job_id}/estimate
+        pass
+
+    def start_job(self):
+        """ Start / queue a job for processing."""
+        # POST /jobs/{job_id}/results
+        pass
+
+    def stop_job(self):
+        """ Stop / cancel job processing."""
+        # DELETE /jobs/{job_id}/results
+        pass
+
+    def list_results(self, type=None):
+        """ Get document with download links."""
+        # GET /jobs/{job_id}/results
+        pass
+
+    def download_results(self, target):
+        """ Download job results."""
+        # GET /jobs/{job_id}/results > ...
+        pass
+
+# TODO: All below methods are depricated (at least not specified in the coreAPI)
     def download(self, outputfile: str, outputformat: str):
         """ Download the result as a raster."""
         pass
