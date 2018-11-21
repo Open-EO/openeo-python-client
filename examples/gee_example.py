a212123f5ef311f5d94a056b01d0cf7d0c537dae
Bernhard Gösswein <bgoesswe@bgoesswe.geo.tuwien.ac.at>
2018-11-21 17:40:48 +0100

    implemented processes, processgraph  and job classes according to client guidelines

--- examples/gee_example.py
+++ examples/gee_example.py
@@ -3,7 +3,7 @@ import logging
 import time
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 GEE_DRIVER_URL = "http://giv-openeo.uni-muenster.de:8080/v0.3"
@@ -19,19 +19,45 @@ password = "test123"
 
 con = openeo.connect(GEE_DRIVER_URL, auth_options={"username": user, "password": password})
 
+#Test Connection
+#print(con.list_processes())
+#print(con.list_collections())
+#print(con.describe_collection("COPERNICUS/S2"))
+
+
 # Test Capabilities
 cap = con.capabilities()
 
-print(cap.version())
-print(cap.list_features())
-print(cap.currency())
-print(cap.list_plans())
+#print(cap.version())
+#print(cap.list_features())
+#print(cap.currency())
+#print(cap.list_plans())
 
+# Test Processes
 
+processes = con.get_processes()
+pg = processes.get_collection(name="COPERNICUS/S2")
+print(pg.graph)
+pg = processes.filter_bbox(pg, west=16.138916, south=48.138600, east=16.524124, north=48.320647, crs="EPSG:4326")
+print(pg.graph)
+pg = processes.filter_daterange(pg, extent=["2017-01-01T00:00:00Z", "2017-01-31T23:59:59Z"])
+print(pg.graph)
+pg = processes.ndvi(pg, nir="B4", red="B8A")
+print(pg.graph)
+pg = processes.min_time(pg)
+print(pg.graph)
 
+# Test Job
+
+job = con.create_job(pg.graph)
+print(job.job_id)
+print(job.start_job())
+print (job.describe_job())
+job.download_results("/tmp/testfile")
 #print(con.describe_collection("COPERNICUS/S2"))
 
 #print(con.describe_process("count_time"))
+
 
 
 
--- openeo/rest/job.py
+++ openeo/rest/job.py
@@ -1,10 +1,11 @@
 from openeo.connection import Connection
+from openeo.processgraph import ProcessGraph
 from openeo.job import Job, JobResult
 from typing import List
 import urllib.request
+import requests
 
-
-class ClientJobResult(JobResult):
+class RESTJobResult(JobResult):
     def __init__(self, url):
         self.url = url
 
@@ -12,12 +13,94 @@ class ClientJobResult(JobResult):
         urllib.request.urlretrieve(self.url, target_file)
 
 
-class ClientJob(Job):
+class RESTJob(Job):
 
     def __init__(self, job_id: str, connection: Connection):
         super().__init__(job_id)
         self.connection = connection
 
+    def describe_job(self):
+        """ Get all job information."""
+        # GET /jobs/{job_id}
+        request = self.connection.get("/jobs/{}".format(self.job_id))
+        return self.connection.parse_json_response(request)
+
+    def update_job(self, process_graph=None, output_format=None,
+                   output_parameters=None, title=None, description=None,
+                   plan=None, budget=None, additional=None):
+        """ Update a job."""
+        # PATCH /jobs/{job_id}
+        pass
+
+    def delete_job(self):
+        """ Delete a job."""
+        # DELETE /jobs/{job_id}
+        request = self.connection.delete("/jobs/{}".format(self.job_id), postdata=None)
+
+        return request.status_code
+
+    def estimate_job(self):
+        """ Calculate an time/cost estimate for a job."""
+        # GET /jobs/{job_id}/estimate
+        request = self.connection.get("/jobs/{}/estimate".format(self.job_id), postdata=None)
+
+        return self.connection.parse_json_response(request)
+
+    def start_job(self):
+        """ Start / queue a job for processing."""
+        # POST /jobs/{job_id}/results
+        request = self.connection.post("/jobs/{}/results".format(self.job_id), postdata=None)
+
+        return request.status_code
+
+    def stop_job(self):
+        """ Stop / cancel job processing."""
+        # DELETE /jobs/{job_id}/results
+        request = self.connection.delete("/jobs/{}/results".format(self.job_id), postdata=None)
+
+        return request.status_code
+        pass
+
+    def list_results(self, type=None):
+        """ Get document with download links."""
+        # GET /jobs/{job_id}/results
+        pass
+
+    def download_results(self, target):
+        """ Download job results."""
+        # GET /jobs/{job_id}/results > ...
+
+
+        download_url = "/jobs/{}/results".format(self.job_id)
+        r = self.connection.get(download_url, stream = True)
+
+        if r.status_code == 200:
+
+            url = r.json()
+            if "links" in url:
+                download_url = url["links"][0]
+                if "href" in download_url:
+                    download_url = download_url["href"]
+
+            auth_header = self.connection.authent.get_header()
+
+            with open(target, 'wb') as handle:
+                response = requests.get(download_url, stream=True, headers=auth_header)
+
+                if not response.ok:
+                    print (response)
+
+                for block in response.iter_content(1024):
+
+                    if not block:
+                        break
+
+                    handle.write(block)
+        else:
+            raise ConnectionAbortedError(r.text)
+        return r.status_code
+
+# TODO: All below methods are depricated (at least not specified in the coreAPI)
     def download(self, outputfile:str, outputformat=None):
         """ Download the result as a raster."""
         try:
@@ -33,6 +116,6 @@ class ClientJob(Job):
         """ Queues the job. """
         return self.connection.queue_job(self.job_id)
 
-    def results(self) -> List[ClientJobResult]:
+    def results(self) -> List[RESTJobResult]:
         """ Returns this job's results. """
-        return [ClientJobResult(link['href']) for link in self.connection.job_results(self.job_id)['links']]
+        return [RESTJobResult(link['href']) for link in self.connection.job_results(self.job_id)['links']]
