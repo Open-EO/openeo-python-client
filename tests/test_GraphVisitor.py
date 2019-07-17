b5479af88ba2720e6004db846d9a2d2c98aebc3a
Stefaan Lippens <stefaan.lippens@vito.be>
2019-07-17 10:17:22 +0200

    Simplify `ProcessGraphVisitor._list_to_graph` and rename to `dereference_from_node_arguments`
    `ProcessGraphVisitor._list_to_graph`:
    - simplify logic
    - Improve docblock and function name (deprecate old name)
    - move related tests from openeo-python-driver

--- tests/test_GraphVisitor.py
+++ tests/test_GraphVisitor.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
-from unittest.mock import MagicMock,call,ANY
+from unittest.mock import MagicMock, call, ANY
+
+import pytest
 
 from openeo.internal.process_graph_visitor import ProcessGraphVisitor
 
@@ -100,3 +102,99 @@ class GraphVisitorTest(TestCase):
         ])
 
         print(leaveProcess)
+
+
+def test_dereference_basic():
+    graph = {
+        "node1": {},
+        "node2": {
+            "arguments": {
+                "data1": {
+                    "from_node": "node1"
+                },
+                "data2": {
+                    "from_node": "node3"
+                }
+            },
+            "result": True
+        },
+        "node3": {
+            "arguments": {
+                "data": {
+                    "from_node": "node4"
+                }
+            }
+        },
+        "node4": {}
+    }
+    result = ProcessGraphVisitor.dereference_from_node_arguments(graph)
+
+    assert result == "node2"
+    assert graph["node1"] == graph["node2"]["arguments"]["data1"]["node"]
+    assert graph["node3"] == graph["node2"]["arguments"]["data2"]["node"]
+    assert graph["node4"] == graph["node3"]["arguments"]["data"]["node"]
+    assert graph == {
+        "node1": {},
+        "node2": {
+            "arguments": {
+                "data1": {"from_node": "node1", "node": {}},
+                "data2": {"from_node": "node3", "node": {
+                    "arguments": {
+                        "data": {"from_node": "node4", "node": {}},
+                    }
+                }},
+            },
+            "result": True
+        },
+        "node3": {
+            "arguments": {
+                "data": {"from_node": "node4", "node": {}},
+            }
+        },
+        "node4": {}
+
+    }
+
+
+def test_dereference_no_result_node():
+    with pytest.raises(ValueError, match="does not contain a result node"):
+        ProcessGraphVisitor.dereference_from_node_arguments({
+            "node1": {},
+            "node2": {}
+        })
+
+
+def test_dereference_invalid_node():
+    graph = {
+        "node1": {},
+        "node2": {
+            "arguments": {
+                "data": {
+                    "from_node": "node3"
+                }
+            },
+            "result": True
+        }
+    }
+    with pytest.raises(ValueError, match="not in process graph"):
+        ProcessGraphVisitor.dereference_from_node_arguments(graph)
+
+
+def test_dereference_cycle():
+    graph = {
+        "node1": {
+            "arguments": {
+                "data": {"from_node": "node2"},
+            },
+            "result": True
+        },
+        "node2": {
+            "arguments": {
+                "data": {"from_node": "node1"},
+            }
+        }
+    }
+    ProcessGraphVisitor.dereference_from_node_arguments(graph)
+    assert graph["node1"]["arguments"]["data"]["node"] is graph["node2"]
+    assert graph["node2"]["arguments"]["data"]["node"] is graph["node1"]
+
