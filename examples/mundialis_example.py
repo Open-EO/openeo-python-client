caf8ee760d17180787d04829afa5528ac0376896
Markus Neteler <neteler@gmail.com>
2019-09-12 09:52:35 +0200

    fix raster to strds

--- examples/mundialis_example.py
+++ examples/mundialis_example.py
@@ -28,7 +28,7 @@ print(cap.list_plans())
 
 # Test Processes
 # North Carolina MODIS LST example
-datacube = con.imagecollection("nc_spm_08.modis_lst.raster.MOD11B3.A2016306.h11v05.single_LST_Day_6km")
+datacube = con.imagecollection("nc_spm_08.modis_lst.strds.MOD11B3.A2016306.h11v05.single_LST_Day_6km")
 datacube = datacube.filter_bbox(west=-80.5, south=32.5, east=-70.0, north=38.6, crs="EPSG:4326")
 datacube = datacube.filter_temporal(extent=["2016-01-01T00:00:00Z", "2016-03-10T23:59:59Z"])
 datacube = datacube.max_time()
