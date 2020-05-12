EP-3402 #123  fix band dimension handling in fuzzy cloud masking use case

tests/rest/datacube/test_bandmath.py
@@ -6,9 +6,9 @@ Band math related tests against both
 
 """
 
+import numpy as np
 import pytest
 
-from openeo.capabilities import ComparableVersion
 from openeo.rest import BandMathException
 from .. import get_download_graph
 from ..conftest import reset_graphbuilder
@@ -278,3 +278,13 @@ def test_merge_cubes_max_resolver(connection, api_version):
     mask = connection.load_collection("MASK")
     merged = s2.merge(mask, overlap_resolver="max")
     assert merged.graph == load_json_resource('data/%s/merge_cubes_max.json' % api_version)
+
+
+def test_fuzzy_mask(connection, api_version):
+    s2 = connection.load_collection("SENTINEL2_SCF")
+    scf_band = s2.band("SCENECLASSIFICATION")
+    clouds = scf_band == 4
+    fuzzy = clouds.apply_kernel(kernel=0.1 * np.ones((3, 3)))
+    mask = fuzzy > 0.3
+    assert mask.graph == load_json_resource('data/%s/fuzzy_mask.json' % api_version)
+