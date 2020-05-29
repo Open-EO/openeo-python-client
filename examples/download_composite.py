349e3ebcff5d1ab47978e4c3ecc5a65c8a994549
Jeroen Dries <jeroen.dries@vito.be>
2020-05-29 15:49:12 +0200

    update basic sample for 1.0

--- examples/download_composite.py
+++ examples/download_composite.py
@@ -1,23 +1,24 @@
-import openeo
 import logging
+
+import openeo
 
 #enable logging in requests library
 logging.basicConfig(level=logging.DEBUG)
 
 #connect with VITO backend
-session = openeo.connect("http://openeo.vgt.vito.be/openeo/0.4.0")
+connection = openeo.connect("https://openeo.vito.be/openeo/1.0")
 
 #retrieve the list of available collections
-collections = session.list_collections()
+collections = connection.list_collections()
 print(collections)
 
 #create image collection
-s2_fapar = session.imagecollection("BIOPAR_FAPAR_V1_GLOBAL")
+s2_fapar = connection.load_collection("BIOPAR_FAPAR_V1_GLOBAL",
+            spatial_extent={'west':16.138916,'east':16.524124,'south':48.1386,'north':48.320647,'crs':'EPSG:4326'},
+            temporal_extent=["2016-01-01","2016-03-10"] )
 
 #specify process graph
 download = s2_fapar \
-    .filter_bbox(west=16.138916, south=48.138600, east=16.524124, north=48.320647, crs="EPSG:4326")  \
-    .filter_daterange(extent=["2016-01-01T00:00:00Z", "2016-03-10T23:59:59Z"]) \
     .max_time() \
     .download("/tmp/openeo-composite.geotiff",format="GeoTiff")
 print(download)
