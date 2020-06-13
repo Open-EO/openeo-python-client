9e520a37d0e9c0e2a23fb5c441aea9242d6755be
Tamas Banyai <tamas.banyai@vito.be>
2020-06-13 11:03:25 +0200

    Added execute_batch to VectorCube

--- openeo/vectorcube.py
+++ openeo/vectorcube.py
@@ -2,6 +2,9 @@ from typing import Dict
 
 from openeo.internal.graph_building import PGNode
 from openeo.metadata import CollectionMetadata
+from typing import Union
+import pathlib
+from openeo.rest.job import RESTJob
 
 
 class VectorCube():
@@ -80,3 +83,43 @@ class VectorCube():
     def download(self, outputfile: str, format: str = "GeoJson", options: dict = None):
         newcollection = self.save_result(format=format, options=options)
         return self._connection.download(newcollection._pg.flatten(), outputfile)
+
+
+    def execute_batch(
+            self,
+            outputfile: Union[str, pathlib.Path], out_format: str = None,
+            print=print, max_poll_interval=60, connection_retry_interval=30,
+            job_options=None, **format_options) -> RESTJob:
+        """
+        Evaluate the process graph by creating a batch job, and retrieving the results when it is finished.
+        This method is mostly recommended if the batch job is expected to run in a reasonable amount of time.
+
+        For very long running jobs, you probably do not want to keep the client running.
+
+        :param job_options:
+        :param outputfile: The path of a file to which a result can be written
+        :param out_format: (optional) Format of the job result.
+        :param format_options: String Parameters for the job result format
+
+        """
+        job = self.send_job(out_format, job_options=job_options, **format_options)
+        return job.run_synchronous(
+            # TODO #135 support multi file result sets too
+            outputfile=outputfile,
+            print=print, max_poll_interval=max_poll_interval, connection_retry_interval=connection_retry_interval
+        )
+
+    def send_job(self, out_format=None, job_options=None, **format_options) -> RESTJob:
+        """
+        Sends a job to the backend and returns a ClientJob instance.
+
+        :param out_format: String Format of the job result.
+        :param job_options:
+        :param format_options: String Parameters for the job result format
+        :return: status: ClientJob resulting job.
+        """
+        shp = self
+        if out_format:
+            # add `save_result` node
+            shp = shp.save_result(format=out_format, options=format_options)
+        return self._connection.create_job(process_graph=shp.graph, additional=job_options)
