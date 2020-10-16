44430c7ac87a323c2a12582769b2e0c057d4d58c
Jeroen Dries <jeroen.dries@vito.be>
2020-10-16 14:49:15 +0200

    fix broken rendering in docs

--- examples/udf/smooth_savitzky_golay.py
+++ examples/udf/smooth_savitzky_golay.py
@@ -1,18 +1,13 @@
-# -*- coding: utf-8 -*-
-# Uncomment the import only for coding support
 from typing import Dict
-
 import xarray
+from scipy.signal import savgol_filter
 from openeo_udf.api.datacube import DataCube
-
 
 def apply_datacube(cube: DataCube, context: Dict) -> DataCube:
     """
     Applies a savitzky-golay smoothing to a timeseries datacube.
     This UDF preserves dimensionality, and assumes a datacube with a temporal dimension 't' as input.
     """
-    from scipy.signal import savgol_filter
-
     array: xarray.DataArray = cube.get_array()
     filled = array.interpolate_na(dim='t')
     smoothed_array = savgol_filter(filled.values, 5, 2, axis=0)
