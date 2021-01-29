b6142ac0398e9188a77266b7780ddc5f25889e83
Stefaan Lippens <stefaan.lippens@vito.be>
2021-01-29 15:15:45 +0100

    Connection: log response headers in DEBUG mode

--- openeo/rest/connection.py
+++ openeo/rest/connection.py
@@ -104,6 +104,8 @@ class RestApiConnection:
             timeout=kwargs.pop("timeout", self.default_timeout),
             **kwargs
         )
+        if _log.isEnabledFor(logging.DEBUG):
+            _log.debug("Got {r} headers {h!r}".format(r=resp, h=resp.headers))
         # Check for API errors and unexpected HTTP status codes as desired.
         status = resp.status_code
         expected_status = ensure_list(expected_status) if expected_status else []
