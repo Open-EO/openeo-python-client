cf6e34065f7b48fcc34ae8de8cba837fcf0de80c
Stefaan Lippens <stefaan.lippens@vito.be>
2021-07-05 12:53:10 +0200

    fix some minor documentation (indentation) issues

--- openeo/api/logs.py
+++ openeo/api/logs.py
@@ -1,23 +1,23 @@
 class LogEntry(dict):
     """
-    LogEntry - log messages for jobs and services
+    Log message and info for jobs and services
 
-    Properties:
-    - id: Unique ID for the log, string, REQUIRED
-    - code: Error code, string, optional
-    - level: Severity level, string (error, warning, info or debug), REQUIRED
-    - message: Error message, string, REQUIRED
-    - time: Date and time of the error event as RFC3339 date-time, string, available since API 1.1.0
-    - path: A "stack trace" for the process, array of dicts
-    - links: Related links, array of dicts
-    - usage: Usage metrics available as property 'usage', dict, available since API 1.1.0
-      May contain the following metrics: cpu, memory, duration, network, disk, storage and other custom ones
-      Each of the metrics is also a dict with the following parts: value (numeric) and unit (string)
-    - data: Arbritrary data the user wants to "log" for debugging purposes.
-      Please note that this property may not exist as there's a difference
-      between None and non-existing. None for example refers to no-data in
-      many cases while the absence of the property means that the user did 
-      not provide any data for debugging.
+    Fields:
+        - ``id``: Unique ID for the log, string, REQUIRED
+        - ``code``: Error code, string, optional
+        - ``level``: Severity level, string (error, warning, info or debug), REQUIRED
+        - ``message``: Error message, string, REQUIRED
+        - ``time``: Date and time of the error event as RFC3339 date-time, string, available since API 1.1.0
+        - ``path``: A "stack trace" for the process, array of dicts
+        - ``links``: Related links, array of dicts
+        - ``usage``: Usage metrics available as property 'usage', dict, available since API 1.1.0
+          May contain the following metrics: cpu, memory, duration, network, disk, storage and other custom ones
+          Each of the metrics is also a dict with the following parts: value (numeric) and unit (string)
+        - ``data``: Arbitrary data the user wants to "log" for debugging purposes.
+          Please note that this property may not exist as there's a difference
+          between None and non-existing. None for example refers to no-data in
+          many cases while the absence of the property means that the user did
+          not provide any data for debugging.
     """
 
     _required = {"id", "level", "message"}
--- openeo/rest/datacube.py
+++ openeo/rest/datacube.py
@@ -381,18 +381,18 @@ class DataCube(ImageCollection, _FromNodeMixin):
         Limits the data cube over the spatial dimensions to the specified geometries.
 
             - For polygons, the filter retains a pixel in the data cube if the point at the pixel center intersects with
-             at least one of the polygons (as defined in the Simple Features standard by the OGC).
+              at least one of the polygons (as defined in the Simple Features standard by the OGC).
             - For points, the process considers the closest pixel center.
             - For lines (line strings), the process considers all the pixels whose centers are closest to at least one
-            point on the line.
+              point on the line.
 
         More specifically, pixels outside of the bounding box of the given geometry will not be available after filtering.
-         All pixels inside the bounding box that are not retained will be set to null (no data).
+        All pixels inside the bounding box that are not retained will be set to null (no data).
 
         :param geometries: One or more geometries used for filtering, specified as GeoJSON in EPSG:4326.
         :return: A data cube restricted to the specified geometries. The dimensions and dimension properties (name,
-        type, labels, reference system and resolution) remain unchanged, except that the spatial dimensions have less
-         (or the same) dimension labels.
+            type, labels, reference system and resolution) remain unchanged, except that the spatial dimensions have less
+            (or the same) dimension labels.
         """
         valid_geojson_types = [
             "Point", "MultiPoint", "LineString", "MultiLineString",
