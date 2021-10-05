666ad3fdbe3787ac3170a65c26f442cf041eff9f
jonathom <jonathanbahlmann@web.de>
2021-10-05 11:29:19 +0200

    adapt tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -464,10 +464,10 @@ def test_get_user_data_dir():
 
 
 def test_guess_format():
-    assert guess_format("./folder/file.nc") == "NetCDF"
-    assert guess_format("./folder/file.tiff") == "GTIFF"
-    assert guess_format("/folder/file.png") == "PNG"
-    assert guess_format("../folder/file.notaformat") == "NOTAFORMAT"
+    assert guess_format("./folder/file.nc") == "netCDF"
+    assert guess_format("./folder/file.tiff") == "GTiff"
+    assert guess_format("/folder/file.png") == "png"
+    assert guess_format("../folder/file.notaformat") == "notaformat"
 
 
 def test_legacy_alias_function(recwarn):
