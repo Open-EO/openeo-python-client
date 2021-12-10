a2cf2afabeb3ee3681ce4be3587d8481718192b3
Jeroen Dries <jeroen.dries@vito.be>
2021-12-10 15:32:47 +0100

    make DataCube visible at the top level

--- openeo/__init__.py
+++ openeo/__init__.py
@@ -27,6 +27,7 @@ class BaseOpenEoException(Exception):
 
 from openeo._version import __version__
 from openeo.imagecollection import ImageCollection
+from openeo.rest.datacube import DataCube
 from openeo.rest.connection import connect, session, Connection
 from openeo.rest.job import RESTJob
 from openeo.internal.graph_building import UDF
