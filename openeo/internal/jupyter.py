8d456c66a712bed20b35c2e9fe0c8c0e57475f6b
Stefaan Lippens <soxofaan@users.noreply.github.com>
2022-01-06 14:09:50 +0100

    Minor style tweak

--- openeo/internal/jupyter.py
+++ openeo/internal/jupyter.py
@@ -89,7 +89,7 @@ def render_component(component: str, data = None, parameters: dict = None):
     
     if component == "data-table":
         parameters['columns'] = TABLE_COLUMNS[parameters['columns']]
-    elif (component in ['collection', 'collections', 'item', 'items']):
+    elif component in ['collection', 'collections', 'item', 'items']:
         url = os.environ.get("OPENEO_BASEMAP_URL")
         attribution = os.environ.get("OPENEO_BASEMAP_ATTRIBUTION")
         parameters['mapOptions'] = {}
@@ -97,7 +97,6 @@ def render_component(component: str, data = None, parameters: dict = None):
             parameters['mapOptions']['basemap'] = url
         if attribution:
             parameters['mapOptions']['attribution'] = attribution
-
 
     # Set the data as the corresponding parameter in the Vue components
     key = COMPONENT_MAP.get(component, component)
