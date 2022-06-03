983d45f7f7df9ec9cf193501163c49dceb6252c9
Stefaan Lippens <stefaan.lippens@vito.be>
2022-06-03 16:49:39 +0200

    Issue #285 improve doc landing page
    Update and move example snippet/result from code to docs

--- openeo/__init__.py
+++ openeo/__init__.py
@@ -1,19 +1,5 @@
 """
-Python Client API for OpenEO backends. This client is a lightweight implementation with limited dependencies on other modules.
-The aim of OpenEO is to process remote sensing data on dedicated processing resources close to the source data.
 
-This client allows users to communicate with OpenEO backends, in a way that feels familiar for Python programmers.
-
-.. literalinclude:: ../examples/download_composite.py
-    :caption: Basic example
-    :name: basic_example
-
-.. image:: ../docs/_static/images/basic_example.png
-  :width: 500
-  :alt: script result
-
-
-In this example, a 'datacube' is created using `load_collection`. Datacubes can be transformed by applying operations directly.
 
 """
 
