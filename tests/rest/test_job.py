d640bdeb796d438a42bef295089a025d14334168
Johan Schreurs <JohanKJSchreurs@users.noreply.github.com>
2023-01-10 10:18:18 +0100

    #352 Add HTTP 502 error to test_execute_batch_with_soft_errors

--- tests/rest/test_job.py
+++ tests/rest/test_job.py
@@ -151,6 +151,10 @@ def test_execute_batch_with_error(con100, requests_mock, tmpdir):
             },
             "Service availability error while polling job status: [503] OidcProviderUnavailable: OIDC Provider is unavailable",
     ),
+    (
+            {"status_code": 502, "text": "Bad Gateway"},
+            "Service availability error while polling job status: [502] unknown: Bad Gateway",
+    ),
 ])
 def test_execute_batch_with_soft_errors(con100, requests_mock, tmpdir, error_response, expected):
     requests_mock.get(API_URL + "/file_formats", json={"output": {"GTiff": {"gis_data_types": ["raster"]}}})
