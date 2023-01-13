b0d100e1a50fa2c14f7b1030b56f66fc7f7c9172
Johan Schreurs <JohanKJSchreurs@users.noreply.github.com>
2023-01-13 16:09:57 +0100

    Issue #361 Moved MultiBackendJobManager into python client

---
+++ openeo/extra/job_management.py
@@ -0,0 +1,263 @@
+import collections
+import contextlib
+import datetime
+import json
+import logging
+import time
+from pathlib import Path
+from typing import Callable, Union, Dict, Optional
+
+import geopandas as gpd
+import pandas as pd
+import requests
+from openeo import Connection, BatchJob
+from openeo.rest import OpenEoApiError
+from openeo.util import deep_get
+
+
+_log = logging.getLogger(__name__)
+
+
+# Container for backend info/settings
+_Backend = collections.namedtuple("_Backend", ["get_connection", "parallel_jobs"])
+
+
+class MultiBackendJobManager:
+    """
+    Tracker for multiple jobs on multiple backends.
+
+    Usage example:
+
+        manager = MultiBackendJobManager()
+        manager.add_backend("foo", connection=openeo.connect("http://foo.test"))
+        manager.add_backend("bar", connection=openeo.connect("http://bar.test"))
+
+        jobs_df = pd.DataFrame(....)
+        output_file = Path("jobs.csv")
+        def start_job(row, connection, **kwargs):
+            ...
+
+        manager.run_jobs(df=df, start_job=start_job, output_file=output_file)
+
+    """
+
+    def __init__(self, poll_sleep: int = 60):
+        """Create a MultiBackendJobManager
+
+        :param poll_sleep: How many seconds to sleep between polls.
+        """
+        self.backends: Dict[str, _Backend] = {}
+        self.poll_sleep = poll_sleep
+
+    def add_backend(
+        self,
+        name: str,
+        connection: Union[Connection, Callable[[], Connection]],
+        parallel_jobs: int = 2,
+    ):
+        """Register a backend with a name and a Connection getter"""
+        if isinstance(connection, Connection):
+            c = connection
+            connection = lambda: c
+        assert callable(connection)
+        self.backends[name] = _Backend(
+            get_connection=connection, parallel_jobs=parallel_jobs
+        )
+
+    def _normalize_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        # check for some required columns.
+        required_with_default = [
+            ("status", "not_started"),
+            ("id", None),
+            ("start_time", None),
+            ("cpu", None),
+            ("memory", None),
+            ("duration", None),
+            ("backend_name", None),
+        ]
+        new_columns = {
+            col: val for (col, val) in required_with_default if col not in df.columns
+        }
+        df = df.assign(**new_columns)
+        # Workaround for loading of geopandas "geometry" column.
+        if "geometry" in df.columns and df["geometry"].dtype.name != "geometry":
+            df["geometry"] = gpd.GeoSeries.from_wkt(df["geometry"])
+        return df
+
+    def run_jobs(self, df: pd.DataFrame, start_job: Callable, output_file: Path):
+        """Runs jobs, specified in a dataframe, and tracks parameters.
+
+        :param df:
+            DataFrame that specifies the jobs, and tracks the jobs' statuses.
+        :param start_job:
+            A callback which will be invoked with the row of the dataframe for which a job should be started.
+        :param output_file:
+            Path to output file (CSV) containing the status and metadata of the jobs.
+        """
+        # TODO: this resume functionality better fits outside of this function
+        #       (e.g. if `output_file` exists: `df` is fully discarded)
+        if output_file.exists() and output_file.is_file():
+            # Resume from existing CSV
+            _log.info(f"Resuming `run_jobs` from {output_file.absolute()}")
+            df = pd.read_csv(output_file)
+            status_histogram = df.groupby("status").size().to_dict()
+            _log.info(f"Status histogram: {status_histogram}")
+
+        df = self._normalize_df(df)
+
+        def persists(df, output_file):
+            df.to_csv(output_file, index=False)
+            _log.info(f"Wrote job metadata to {output_file.absolute()}")
+
+        while (
+            df[
+                (df.status != "finished")
+                & (df.status != "skipped")
+                & (df.status != "start_failed")
+            ].size
+            > 0
+        ):
+            with ignore_connection_errors(context="get statuses"):
+                self._update_statuses(df)
+            status_histogram = df.groupby("status").size().to_dict()
+            _log.info(f"Status histogram: {status_histogram}")
+            persists(df, output_file)
+
+            if len(df[df.status == "not_started"]) > 0:
+                # Check number of jobs running at each backend
+                running = df[
+                    (df.status == "created")
+                    | (df.status == "queued")
+                    | (df.status == "running")
+                ]
+                per_backend = running.groupby("backend_name").size().to_dict()
+                _log.info(f"Running per backend: {per_backend}")
+                for backend_name in self.backends:
+                    backend_load = per_backend.get(backend_name, 0)
+                    if backend_load < self.backends[backend_name].parallel_jobs:
+                        to_add = (
+                            self.backends[backend_name].parallel_jobs - backend_load
+                        )
+                        to_launch = df[df.status == "not_started"].iloc[0:to_add]
+                        for i in to_launch.index:
+                            df.loc[i, "backend_name"] = backend_name
+                            row = df.loc[i]
+                            try:
+                                _log.info(
+                                    f"Starting job on backend {backend_name} for {row.to_dict()}"
+                                )
+                                job = start_job(
+                                    row=row,
+                                    connection_provider=self.backends[
+                                        backend_name
+                                    ].get_connection,
+                                    connection=self.backends[
+                                        backend_name
+                                    ].get_connection(),
+                                    provider=backend_name,
+                                )
+                            except requests.exceptions.ConnectionError as e:
+                                _log.warning(
+                                    f"Failed to start job for {row.to_dict()}",
+                                    exc_info=True,
+                                )
+                                df.loc[i, "status"] = "start_failed"
+                            else:
+                                df.loc[
+                                    i, "start_time"
+                                ] = datetime.datetime.now().isoformat()
+                                if job:
+                                    df.loc[i, "id"] = job.job_id
+                                    with ignore_connection_errors(context="get status"):
+                                        status = job.status()
+                                        df.loc[i, "status"] = status
+                                        if status == "created":
+                                            # start job if not yet done by callback
+                                            try:
+                                                job.start_job()
+                                                df.loc[i, "status"] = job.status()
+                                            except OpenEoApiError as e:
+                                                _log.error(e)
+                                                df.loc[i, "status"] = "start_failed"
+                                else:
+                                    df.loc[i, "status"] = "skipped"
+
+                            persists(df, output_file)
+
+            time.sleep(self.poll_sleep)
+
+    def on_job_done(self, job: BatchJob, row):
+        """
+        Handles jobs that have finished. Can be overridden to provide custom behaviour.
+
+        Default implementation downloads the results into a folder containing the title.
+
+        :param job: the job that has finished.
+        :param row: DataFrame row containing the job's metadata.
+        """
+        # TODO: param `row` is never accessed in this method. Remove it? Is this intended for future use?
+
+        job_metadata = job.describe_job()
+        job.get_results().download_files(target=job_metadata["title"])
+        with open(Path(job_metadata["title"]) / f"job_{job.job_id}.json", "w") as f:
+            json.dump(job_metadata, f, ensure_ascii=False)
+
+    def on_job_error(self, job: BatchJob, row):
+        logs = job.logs()
+        error_logs = [l for l in logs if l.level.lower() == "error"]
+        job_metadata = job.describe_job()
+
+        title = job_metadata["title"]
+        if len(error_logs) > 0:
+            (f"job_{title}_errors.json").write_text(json.dumps(error_logs, indent=2))
+
+    def _update_statuses(self, df: pd.DataFrame):
+        """Update status (and stats) of running jobs (in place)"""
+        active = df.loc[
+            (df.status == "created")
+            | (df.status == "queued")
+            | (df.status == "running")
+        ]
+        for i in active.index:
+            job_id = df.loc[i, "id"]
+            backend_name = df.loc[i, "backend_name"]
+
+            try:
+                con = self.backends[backend_name].get_connection()
+                the_job = con.job(job_id)
+                job_metadata = the_job.describe_job()
+                _log.info(
+                    f"Status of job {job_id!r} (on backend {backend_name}) is {job_metadata['status']!r}"
+                )
+                if (
+                    df.loc[i, "status"] == "running"
+                    and job_metadata["status"] == "finished"
+                ):
+                    self.on_job_done(the_job, df.loc[i])
+                if df.loc[i, "status"] != "error" and job_metadata["status"] == "error":
+                    self.on_job_error(the_job, df.loc[i])
+
+                df.loc[i, "status"] = job_metadata["status"]
+                for key in job_metadata.get("usage", {}).keys():
+                    df.loc[i, key] = _format_usage_stat(job_metadata, key)
+
+            except OpenEoApiError as e:
+                print(f"error for {backend_name}")
+                print(e)
+
+
+def _format_usage_stat(job_metadata: dict, field: str) -> str:
+    value = deep_get(job_metadata, "usage", field, "value", default=0)
+    unit = deep_get(job_metadata, "usage", field, "unit", default="")
+    return f"{value} {unit}".strip()
+
+
+@contextlib.contextmanager
+def ignore_connection_errors(context: Optional[str] = None):
+    """Context manager to ignore connection errors."""
+    try:
+        yield
+    except requests.exceptions.ConnectionError as e:
+        _log.warning(f"Ignoring connection error (context {context or 'n/a'}): {e}")
+        # Back off a bit
+        time.sleep(5)
---
+++ tests/extra/test_job_management.py
@@ -0,0 +1,110 @@
+import openeo
+import pandas as pd
+from openeo.rest.job import RESTJob
+
+from openeo.extra.job_management import MultiBackendJobManager
+
+import pytest
+import os
+
+from pathlib import Path
+
+
+@pytest.fixture
+def temp_working_dir(tmp_path):
+    """Use a temporary working directory, because some tests generate files
+    in the working directory, and that pollutes your git work tree.
+
+    Preventing it would be better, but sometimes that is difficult.
+    TODO: What if we want to keep those files for inspecting after the test run, but preferably in a separate folder?
+    """
+
+    orig_workdir: Path = Path.cwd()
+    # Make the CWD a bit more specific because test can use tmp_dir for other things too.
+    temp_word_dir: Path = tmp_path / "test-workdir"
+    if not temp_word_dir.exists():
+        temp_word_dir.mkdir()
+    os.chdir(temp_word_dir)
+
+    yield temp_word_dir
+
+    os.chdir(orig_workdir)
+
+
+class TestMultiBackendJobManager:
+    def test_basic(self, tmp_path, requests_mock, temp_working_dir):
+        requests_mock.get("http://foo.test/", json={"api_version": "1.1.0"})
+        requests_mock.get("http://bar.test/", json={"api_version": "1.1.0"})
+
+        def mock_job_status(job_id, queued=1, running=2):
+            """Mock job status polling sequence"""
+            response_list = sum(
+                [
+                    [
+                        {
+                            "json": {
+                                "id": job_id,
+                                "title": f"Job {job_id}",
+                                "status": "queued",
+                            }
+                        }
+                    ]
+                    * queued,
+                    [
+                        {
+                            "json": {
+                                "id": job_id,
+                                "title": f"Job {job_id}",
+                                "status": "running",
+                            }
+                        }
+                    ]
+                    * running,
+                    [
+                        {
+                            "json": {
+                                "id": job_id,
+                                "title": f"Job {job_id}",
+                                "status": "finished",
+                            }
+                        }
+                    ],
+                ],
+                [],
+            )
+            for backend in ["http://foo.test", "http://bar.test"]:
+                requests_mock.get(f"{backend}/jobs/{job_id}", response_list)
+                # It also needs the job results endpoint, though that can be a dummy implementation.
+                # When the job is finished the system tries to download the results and that is what
+                # needs this endpoint.
+                requests_mock.get(
+                    f"{backend}/jobs/{job_id}/results", json={"links": []}
+                )
+
+        mock_job_status("job-2018", queued=1, running=2)
+        mock_job_status("job-2019", queued=2, running=3)
+        mock_job_status("job-2020", queued=3, running=4)
+        mock_job_status("job-2021", queued=3, running=5)
+        mock_job_status("job-2022", queued=5, running=6)
+
+        manager = MultiBackendJobManager(poll_sleep=0.2)
+        manager.add_backend("foo", connection=openeo.connect("http://foo.test"))
+        manager.add_backend("bar", connection=openeo.connect("http://bar.test"))
+
+        df = pd.DataFrame(
+            {
+                "year": [2018, 2019, 2020, 2021, 2022],
+            }
+        )
+        output_file = tmp_path / "jobs.csv"
+
+        def start_job(row, connection, **kwargs):
+            year = row["year"]
+            return RESTJob(job_id=f"job-{year}", connection=connection)
+
+        manager.run_jobs(df=df, start_job=start_job, output_file=output_file)
+
+        result = pd.read_csv(output_file)
+        assert len(result) == 5
+        assert set(result.status) == {"finished"}
+        assert set(result.backend_name) == {"foo", "bar"}
