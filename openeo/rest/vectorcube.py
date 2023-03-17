50d6ffdb5e6c84277e43da3e9626c92129a773eb
Jeroen Dries <jeroen.dries@vito.be>
2023-03-17 07:51:09 +0100

    add croptype dir to publish layer

--- openeo/rest/vectorcube.py
+++ openeo/rest/vectorcube.py
@@ -110,7 +110,7 @@ class VectorCube(_ProcessGraphAbstraction):
 
     def execute_batch(
             self,
-            outputfile: Union[str, pathlib.Path], out_format: str = None,
+            outputfile: Union[str, pathlib.Path] = None, out_format: str = None,
             print=print, max_poll_interval=60, connection_retry_interval=30,
             job_options=None, **format_options) -> BatchJob:
         """
