c5d544b77dba4acd3064c63d70dd3ed04c5a000f
clausmichele <claus.michele@hotmail.it>
2023-06-01 09:50:33 +0200

    added resample_cube_spatial implementation

--- openeo/local/processing.py
+++ openeo/local/processing.py
@@ -60,3 +60,73 @@ PROCESS_REGISTRY["load_collection"] = Process(
     spec=openeo_processes_dask.specs.load_collection, 
     implementation=load_local_collection,
 )
+
+def resample_cube_spatial_rioxarray(*args, **kwargs):
+    pretty_args = {k: type(v) for k, v in kwargs.items()}
+    _log.debug(f"Running process resample_cube_spatial")
+    _log.debug(f"kwargs: {pretty_args}")
+    _log.debug("-" * 80)
+
+    methods_list = {
+        "near": 0,
+        "bilinear": 1,
+        "cubic": 2,
+        "cubicspline": 3,
+        "lanczos": 4,
+        "average": 5,
+        "mode": 6,
+        "gauss": 7,
+        "max": 8,
+        "min": 9,
+        "med": 10,
+        "q1": 11,
+        "q3": 12,
+        "sum": 13,
+        "rms": 14
+    }
+    data = kwargs['data']
+    target = kwargs['target']
+    method = kwargs['method']
+    # The target doesn't need to have the same dimensions as the data
+    input_data_dims = (
+        data.openeo.band_dims + data.openeo.temporal_dims + data.openeo.spatial_dims + data.openeo.other_dims
+    )
+
+    if method not in methods_list.keys():
+        raise Exception(
+            f'Selected resampling method "{method}" is not available! Please select one of '
+            f"[{', '.join(methods_list.keys())}]"
+        )
+    if len(data.openeo.temporal_dims) > 0:
+        for i,t in enumerate(data[data.openeo.temporal_dims[0]]):
+            if i == 0:
+                resampled_data = data.loc[{data.openeo.temporal_dims[0]:t}].rio.reproject_match(
+                    target, resampling=methods_list[method]
+                )
+                resampled_data = resampled_data.assign_coords({data.openeo.temporal_dims[0]:t}).expand_dims(data.openeo.temporal_dims[0])
+            else:
+                tmp = data.loc[{data.openeo.temporal_dims[0]:t}].rio.reproject_match(
+                    target, resampling=methods_list[method]
+                )
+                tmp = tmp.assign_coords({data.openeo.temporal_dims[0]:t}).expand_dims(data.openeo.temporal_dims[0])
+                resampled_data = xr.concat([resampled_data,tmp],dim=data.openeo.temporal_dims[0])
+    else:
+        resampled_data = data.rio.reproject_match(
+            target, resampling=method
+        )
+    resampled_data.rio.write_crs(target.rio.crs, inplace=True)
+
+    # Order axes back to how they were before
+    resampled_data = resampled_data.transpose(*data.dims)
+
+    # Ensure that attrs except crs are copied over
+    for k, v in data.attrs.items():
+        if k.lower() != "crs":
+            resampled_data.attrs[k] = v
+    return resampled_data
+
+
+PROCESS_REGISTRY["resample_cube_spatial"] = Process(
+    spec=openeo_processes_dask.specs.resample_cube_spatial,
+    implementation=resample_cube_spatial_rioxarray,
+)
