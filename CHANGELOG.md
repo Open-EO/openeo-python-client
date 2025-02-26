4f241070df66f12586ad56a6b3e2c078d47927be
Stefaan Lippens <stefaan.lippens@vito.be>
2025-02-26 12:24:48 +0100

    Issue #742 fix legacy save_result/export_workspace usage pattern

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+- Fix legacy usage pattern to append `export_workspace` to `save_result` with generic `process()` helper method ([#742](https://github.com/Open-EO/openeo-python-client/issues/742))
+
 
 ## [0.39.0] - 2025-02-25
 
--- openeo/rest/stac_resource.py
+++ openeo/rest/stac_resource.py
@@ -36,6 +36,24 @@ class StacResource(_ProcessGraphAbstraction):
     def __init__(self, graph: PGNode, connection: Optional[Connection] = None):
         super().__init__(pgnode=graph, connection=connection)
 
+    def process(
+        self,
+        process_id: str,
+        arguments: Optional[dict] = None,
+        namespace: Optional[str] = None,
+        **kwargs,
+    ) -> StacResource:
+        """
+        Generic helper to create a new StacResource by applying a process.
+
+        :param process_id: process id of the process.
+        :param arguments: argument dictionary for the process.
+        :param namespace: optional: process namespace
+        """
+        pg = self._build_pgnode(process_id=process_id, arguments=arguments, namespace=namespace, **kwargs)
+        # TODO: warn that actual return type can not or is not properly detected
+        return StacResource(graph=pg, connection=self._connection)
+
     @openeo_process
     def export_workspace(self, workspace: str, merge: Union[str, None] = None) -> StacResource:
         """
