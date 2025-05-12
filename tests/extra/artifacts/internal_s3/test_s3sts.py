22fbf32e2662efae2927bb9a1836022c8af69205
Peter Van Bouwel <peter.vanbouwel@vito.be>
2025-05-12 16:28:36 +0200

    tests: do not break tests in older Python versions

--- tests/extra/artifacts/internal_s3/test_s3sts.py
+++ tests/extra/artifacts/internal_s3/test_s3sts.py
@@ -23,7 +23,8 @@ fake_creds_response: AssumeRoleWithWebIdentityResponseTypeDef = {
         "AccessKeyId": "akid",
         "SecretAccessKey": "secret",
         "SessionToken": "token",
-        "Expiration": datetime.datetime.now(tz=datetime.UTC) + datetime.timedelta(hours=1),
+        # TODO: go for datetime.datetime.now(tz=datetime.UTC) once 3.10 support is no longer needed
+        "Expiration": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
     },
     "SubjectFromWebIdentityToken": "tokensubject",
     "AssumedRoleUser": {"AssumedRoleId": "1", "Arn": "not important"},
