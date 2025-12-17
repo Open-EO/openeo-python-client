81d417803ee0ec0d56ecd57629dae1cad73ce591
Hans Vanrompay <hansvanrompay@hotmail.com>
2025-12-17 10:30:01 +0100

    remove todo

--- openeo/extra/job_management/_manager.py
+++ openeo/extra/job_management/_manager.py
@@ -543,7 +543,6 @@ class MultiBackendJobManager:
 
         if len(not_started) > 0 and len(pending) < 10:
             # Check number of jobs running at each backend
-            # TODO: should "created" be included in here? Calling this "running" is quite misleading then.
             running = job_db.get_by_status(statuses=["running"])
             stats["job_db get_by_status"] += 1
             per_backend = running.groupby("backend_name").size().to_dict()
