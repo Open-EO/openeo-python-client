e6230a314abc5bd1b842cf05820bd5a7fa2f63d6
niebl <caro.niebl@uni-muenster.de>
2026-02-18 11:27:18 +0100

    revert test_testing.py to af55fd68312c6e0b1bf7c819d576d7d0ec32e959

--- tests/rest/test_testing.py
+++ tests/rest/test_testing.py
@@ -7,12 +7,9 @@ from openeo.rest._testing import DummyBackend
 
 
 @pytest.fixture
-def dummy_backend120(requests_mock, con120):
+def dummy_backend(requests_mock, con120):
     return DummyBackend(requests_mock=requests_mock, connection=con120)
 
-@pytest.fixture
-def dummy_backend130(requests_mock, con130):
-    return DummyBackend(requests_mock=requests_mock, connection=con130)
 
 DUMMY_PG_ADD35 = {
     "add35": {"process_id": "add", "arguments": {"x": 3, "y": 5}, "result": True},
@@ -20,10 +17,10 @@ DUMMY_PG_ADD35 = {
 
 
 class TestDummyBackend:
-    def test_create_job(self, dummy_backend120, con120):
-        assert dummy_backend120.batch_jobs == {}
+    def test_create_job(self, dummy_backend, con120):
+        assert dummy_backend.batch_jobs == {}
         _ = con120.create_job(DUMMY_PG_ADD35)
-        assert dummy_backend120.batch_jobs == {
+        assert dummy_backend.batch_jobs == {
             "job-000": {
                 "job_id": "job-000",
                 "pg": {"add35": {"process_id": "add", "arguments": {"x": 3, "y": 5}, "result": True}},
@@ -31,33 +28,33 @@ class TestDummyBackend:
             }
         }
 
-    def test_start_job(self, dummy_backend120, con120):
+    def test_start_job(self, dummy_backend, con120):
         job = con120.create_job(DUMMY_PG_ADD35)
-        assert dummy_backend120.batch_jobs == {
+        assert dummy_backend.batch_jobs == {
             "job-000": {"job_id": "job-000", "pg": DUMMY_PG_ADD35, "status": "created"},
         }
         job.start()
-        assert dummy_backend120.batch_jobs == {
+        assert dummy_backend.batch_jobs == {
             "job-000": {"job_id": "job-000", "pg": DUMMY_PG_ADD35, "status": "finished"},
         }
 
-    def test_job_status_updater_error(self, dummy_backend120, con120):
-        dummy_backend120.job_status_updater = lambda job_id, current_status: "error"
+    def test_job_status_updater_error(self, dummy_backend, con120):
+        dummy_backend.job_status_updater = lambda job_id, current_status: "error"
 
         job = con120.create_job(DUMMY_PG_ADD35)
-        assert dummy_backend120.batch_jobs["job-000"]["status"] == "created"
+        assert dummy_backend.batch_jobs["job-000"]["status"] == "created"
         job.start()
-        assert dummy_backend120.batch_jobs["job-000"]["status"] == "error"
+        assert dummy_backend.batch_jobs["job-000"]["status"] == "error"
 
     @pytest.mark.parametrize("final", ["finished", "error"])
-    def test_setup_simple_job_status_flow(self, dummy_backend120, con120, final):
-        dummy_backend120.setup_simple_job_status_flow(queued=2, running=3, final=final)
+    def test_setup_simple_job_status_flow(self, dummy_backend, con120, final):
+        dummy_backend.setup_simple_job_status_flow(queued=2, running=3, final=final)
         job = con120.create_job(DUMMY_PG_ADD35)
-        assert dummy_backend120.batch_jobs["job-000"]["status"] == "created"
+        assert dummy_backend.batch_jobs["job-000"]["status"] == "created"
 
         # Note that first status update (to "queued" here) is triggered from `start()`, not `status()` like below
         job.start()
-        assert dummy_backend120.batch_jobs["job-000"]["status"] == "queued"
+        assert dummy_backend.batch_jobs["job-000"]["status"] == "queued"
 
         # Now go through rest of status flow, through `status()` calls
         assert job.status() == "queued"
@@ -69,25 +66,25 @@ class TestDummyBackend:
         assert job.status() == final
         assert job.status() == final
 
-    def test_setup_simple_job_status_flow_final_per_job(self, dummy_backend120, con120):
+    def test_setup_simple_job_status_flow_final_per_job(self, dummy_backend, con120):
         """Test per-job specific final status"""
-        dummy_backend120.setup_simple_job_status_flow(
+        dummy_backend.setup_simple_job_status_flow(
             queued=2, running=3, final="finished", final_per_job={"job-001": "error"}
         )
         job0 = con120.create_job(DUMMY_PG_ADD35)
         job1 = con120.create_job(DUMMY_PG_ADD35)
         job2 = con120.create_job(DUMMY_PG_ADD35)
-        assert dummy_backend120.batch_jobs["job-000"]["status"] == "created"
-        assert dummy_backend120.batch_jobs["job-001"]["status"] == "created"
-        assert dummy_backend120.batch_jobs["job-002"]["status"] == "created"
+        assert dummy_backend.batch_jobs["job-000"]["status"] == "created"
+        assert dummy_backend.batch_jobs["job-001"]["status"] == "created"
+        assert dummy_backend.batch_jobs["job-002"]["status"] == "created"
 
         # Note that first status update (to "queued" here) is triggered from `start()`, not `status()` like below
         job0.start()
         job1.start()
         job2.start()
-        assert dummy_backend120.batch_jobs["job-000"]["status"] == "queued"
-        assert dummy_backend120.batch_jobs["job-001"]["status"] == "queued"
-        assert dummy_backend120.batch_jobs["job-002"]["status"] == "queued"
+        assert dummy_backend.batch_jobs["job-000"]["status"] == "queued"
+        assert dummy_backend.batch_jobs["job-001"]["status"] == "queued"
+        assert dummy_backend.batch_jobs["job-002"]["status"] == "queued"
 
         # Now go through rest of status flow, through `status()` calls
         for expected_status in ["queued", "running", "running", "running"]:
@@ -101,23 +98,9 @@ class TestDummyBackend:
             assert job1.status() == "error"
             assert job2.status() == "finished"
 
-    def test_setup_job_start_failure(self, dummy_backend120):
-        job = dummy_backend120.connection.create_job(process_graph={})
-        dummy_backend120.setup_job_start_failure()
+    def test_setup_job_start_failure(self, dummy_backend):
+        job = dummy_backend.connection.create_job(process_graph={})
+        dummy_backend.setup_job_start_failure()
         with pytest.raises(OpenEoApiError, match=re.escape("[500] Internal: No job starting for you, buddy")):
             job.start()
         assert job.status() == "error"
-
-    def test_version(self, dummy_backend120, dummy_backend130):
-        capabilities120 = dummy_backend120.connection.capabilities()
-        capabilities130 = dummy_backend130.connection.capabilities()
-
-        assert capabilities120.api_version() == "1.2.0"
-        assert capabilities130.api_version() == "1.3.0"
-
-    def test_jwt_conformance(self, dummy_backend120, dummy_backend130):
-        capabilities120 = dummy_backend120.connection.capabilities()
-        capabilities130 = dummy_backend130.connection.capabilities()
-
-        assert capabilities120.has_conformance("https://api.openeo.org/*/authentication/jwt") == False
-        assert capabilities130.has_conformance("https://api.openeo.org/*/authentication/jwt") == True
